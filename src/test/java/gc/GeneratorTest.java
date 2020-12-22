2a145a0d2a5aa4abfc57602193399543d5a61102
xocer <vitamin9292@mail.ru>
2020-12-22 13:39:17 +0300

    3. Шаблонизатор. [#380706]

---
+++ src/test/java/gc/GeneratorTest.java
@@ -0,0 +1,24 @@
+package gc;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenProduceNormWork() {
+        Generator generator = new Generator();
+        String result = generator.produce("наша строка", new HashMap<String, String>());
+        assertThat(result, is(new String("ожидаемая строка")));
+    }
+
+    @Test(expected = Exception.class)
+    public void whenProduceKeysHaveProblem() {
+        Generator generator = new Generator();
+        String result = generator.produce("наша строка", new HashMap<String, String>());
+    }
+}
