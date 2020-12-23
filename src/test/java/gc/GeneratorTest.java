c432995fa41c51595a1151d416a1076a23037179
xocer <vitamin9292@mail.ru>
2020-12-23 15:18:45 +0300

    3. Шаблонизатор. [#380706] 2.0

--- src/test/java/gc/GeneratorTest.java
+++ src/test/java/gc/GeneratorTest.java
@@ -12,13 +12,13 @@ public class GeneratorTest {
     @Test
     public void whenProduceNormWork() {
         Generator generator = new Generator();
-        String result = generator.produce("наша строка", new HashMap<String, String>());
+        String result = generator.produce("I am a ${name}, Who are ${subject}? ", new HashMap<String, String>());
         assertThat(result, is(new String("ожидаемая строка")));
     }
 
     @Test(expected = Exception.class)
     public void whenProduceKeysHaveProblem() {
         Generator generator = new Generator();
-        String result = generator.produce("наша строка", new HashMap<String, String>());
+        String result = generator.produce("I am a ${name}, Who are ${subject}? ", new HashMap<String, String>());
     }
 }
