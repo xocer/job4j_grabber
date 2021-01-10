8da59dafaa9a1edde64dc43fcafad3c2f86d5d8a
xocer <vitamin9292@mail.ru>
2021-01-10 21:27:23 +0300

    1. Хранилище продуктов [#380724] 2.0

--- src/test/java/ood/lsp/SimpleParkingTest.java
+++ src/test/java/ood/lsp/SimpleParkingTest.java
@@ -12,34 +12,63 @@ import static org.junit.Assert.assertThat;
 public class SimpleParkingTest {
 
     @Test
-    public void whenAddNormal() {
+    public void whenAddCarsNormal() {
         Parking parking = new SimpleParking(10, 5);
-        parking.add(new Car());
-        parking.add(new TruckCar());
-        assertThat(parking.getFreePlaces(), is(13));
+        parking.add(new Car(321));
+        parking.add(new Car(2));
+        parking.add(new Car(44));
+        assertThat(parking.getFreeCarsPlaces(), is(7));
     }
 
     @Test
-    public void whenRemoveNormal() {
+    public void whenAddTruckCarsNormal() {
         Parking parking = new SimpleParking(10, 5);
-        parking.add(new Car());
-        parking.add(new TruckCar());
+        parking.add(new Car(321));
+        parking.add(new Car(2));
+        parking.add(new Car(44));
+        assertThat(parking.getFreeCarsPlaces(), is(2));
+    }
+
+    @Test
+    public void whenRemoveCarsNormal() {
+        Parking parking = new SimpleParking(10, 5);
+        parking.add(new Car(321));
+        parking.add(new Car(2));
+        parking.add(new Car(44));
         parking.remove(2);
-        assertThat(parking.getFreePlaces(), is(14));
+        assertThat(parking.getFreeCarsPlaces(), is(8));
+    }
+
+    @Test
+    public void whenRemoveTruckCarsNormal() {
+        Parking parking = new SimpleParking(10, 5);
+        parking.add(new TruckCar(321, 4));
+        parking.add(new TruckCar(2, 5));
+        parking.add(new TruckCar(44, 3));
+        parking.remove(2);
+        assertThat(parking.getFreeCarsPlaces(), is(3));
     }
 
     @Test(expected = Exception.class)
     public void whenAddFail() {
         Parking parking = new SimpleParking(10, 0);
-        parking.add(new Car());
-        parking.add(new TruckCar());
+        parking.add(new Car(44));
+        parking.add(new TruckCar(4, 2));
     }
 
     @Test(expected = Exception.class)
     public void whenRemoveFail() {
         Parking parking = new SimpleParking(10, 5);
-        parking.add(new Car());
-        parking.add(new TruckCar());
-        parking.remove(2);
+        parking.add(new Car(22));
+        parking.add(new TruckCar(6, 5));
+        parking.remove(22);
+    }
+
+    @Test
+    public void whenTruckCarAddToCarsParking() {
+        Parking parking = new SimpleParking(10, 0);
+        parking.add(new Car(22));
+        parking.add(new TruckCar(6, 5));
+        assertThat(parking.getFreeCarsPlaces(), is(4));
     }
 }
