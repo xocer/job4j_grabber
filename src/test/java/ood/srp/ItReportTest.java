583205cc2ac0421c38b7378ac8268448d4ed735b
xocer <vitamin9292@mail.ru>
2021-01-14 13:17:41 +0300

    test jitpack

--- src/test/java/ood/srp/ItReportTest.java
+++ src/test/java/ood/srp/ItReportTest.java
@@ -1,39 +1,39 @@
-//package ood.srp;
-//
-//import org.junit.Test;
-//import ru.job4j.ood.srp.Employee;
-//import ru.job4j.ood.srp.ItReport;
-//import ru.job4j.ood.srp.MemStore;
-//import ru.job4j.ood.srp.ReportEngine;
-//
-//import java.util.Calendar;
-//
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.assertThat;
-//
-//public class ItReportTest {
-//    @Test
-//    public void whenItReportGenerated() {
-//        MemStore store = new MemStore();
-//        Calendar now = Calendar.getInstance();
-//        Employee worker = new Employee("Ivan", now, now, 100);
-//        store.add(worker);
-//        ItReport engine = new ItReport(store);
-//        ReportEngine reportEngine = new ReportEngine(engine);
-//        StringBuilder expect = new StringBuilder()
-//                .append("Name; Hired; Fired; Salary;")
-//                .append(System.lineSeparator())
-//                .append("<body>")
-//                .append(System.lineSeparator())
-//                .append("<p").append(worker.getName()).append("/p>")
-//                .append(System.lineSeparator())
-//                .append("<p").append(worker.getHired()).append("/p>")
-//                .append(System.lineSeparator())
-//                .append("<p").append(worker.getFired()).append("/p>")
-//                .append(System.lineSeparator())
-//                .append("<p").append(worker.getSalary()).append("/p>")
-//                .append(System.lineSeparator())
-//                .append("</body");
-//        assertThat(reportEngine.generate(em -> true), is(expect.toString()));
-//    }
-//}
+package ood.srp;
+
+import org.junit.Test;
+import ru.job4j.ood.srp.Employee;
+import ru.job4j.ood.srp.ItReport;
+import ru.job4j.ood.srp.MemStore;
+import ru.job4j.ood.srp.ReportEngine;
+
+import java.util.Calendar;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ItReportTest {
+    @Test
+    public void whenItReportGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        ItReport engine = new ItReport(store);
+        ReportEngine reportEngine = new ReportEngine(engine);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append("<body>")
+                .append(System.lineSeparator())
+                .append("<p").append(worker.getName()).append("/p>")
+                .append(System.lineSeparator())
+                .append("<p").append(worker.getHired()).append("/p>")
+                .append(System.lineSeparator())
+                .append("<p").append(worker.getFired()).append("/p>")
+                .append(System.lineSeparator())
+                .append("<p").append(worker.getSalary()).append("/p>")
+                .append(System.lineSeparator())
+                .append("</body");
+        assertThat(reportEngine.generate(em -> true), is(expect.toString()));
+    }
+}
