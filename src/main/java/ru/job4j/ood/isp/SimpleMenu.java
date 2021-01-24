75d956556035280768f85ace13d10338d5c99ffa
xocer <vitamin9292@mail.ru>
2021-01-24 14:16:43 +0300

    Создать меню. [#380728]

---
+++ src/main/java/ru/job4j/ood/isp/SimpleMenu.java
@@ -0,0 +1,29 @@
+package ru.job4j.ood.isp;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface SimpleMenu {
+    boolean add(String parent, String child);
+
+    Optional<Node> findBy(String value);
+
+    @Data
+    class Node {
+        String value;
+        final List<Node> children = new ArrayList<>();
+
+        public Node(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+}
---
+++ src/main/java/ru/job4j/ood/isp/ViewController.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.isp;
+
+public interface ViewController extends SimpleMenu{
+    void print(Node node, int level );
+}
