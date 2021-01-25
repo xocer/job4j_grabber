71d952c3034b033bdbdd8d45867fbb8d73634bcc
xocer <vitamin9292@mail.ru>
2021-01-25 23:02:41 +0300

    1. Экзамен. GC. [#380744]

--- src/main/java/ru/job4j/gc/jmh/Article.java
+++ src/main/java/ru/job4j/gc/jmh/Article.java
@@ -1,14 +1,16 @@
 package ru.job4j.gc.jmh;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.IOException;
 
 public class Article {
 
-    public Article() {
-    }
 
-    @Benchmark
+
     public static boolean generateBy(String origin, String line) {
         String[] split = origin.split("[^a-zA-Zа-яёА-ЯЁ]");
         String[] newText = line.split("[^a-zA-Zа-яёА-ЯЁ]");
@@ -18,5 +20,40 @@ public class Article {
             }
         }
         return true;
+    }
+
+    public static boolean generateBy2(String origin, String line) {
+        String[] split = origin.split("[^a-zA-Zа-яёА-ЯЁ]");
+        String[] newText = line.split("[^a-zA-Zа-яёА-ЯЁ]");
+        int count = 0;
+        for (int i = 0; i < newText.length; i++) {
+            for (int j = 0; j < split.length; j++) {
+                if (split[j].equals(newText[i])) {
+                    count = 0;
+                    break;
+                }
+                count++;
+                if (count == split.length) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Benchmark
+    public void testBenchmark () {
+        generateBy("Мама мыла раму и окно",
+                "мыла пол");
+    }
+
+    @Benchmark
+    public void testBenchmark2() {
+        generateBy2("Мама мыла раму и окно",
+                "мыла пол");
+    }
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        Main.main(args);
     }
 }
