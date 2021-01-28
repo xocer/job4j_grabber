1e28d23ddca82356f15d8670618497628144dda3
xocer <vitamin9292@mail.ru>
2021-01-28 19:04:10 +0300

    1.2. Режим ожидания. [#458452]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread loading = new Thread(() ->
+        {
+            for (int i = 0; i < 100; i++) {
+                System.out.print("\rLoading : " + i + "%");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        loading.start();
+    }
+}
