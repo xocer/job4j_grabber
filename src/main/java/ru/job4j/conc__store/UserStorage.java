222e7e61c80c43c83f737a7e9efe42b08d2d54cc
xocer <vitamin9292@mail.ru>
2021-02-01 19:20:06 +0300

    3. Класс хранилища пользователей UserStorage [#458466]

--- src/main/java/ru/job4j/conc__store/UserStorage.java
+++ src/main/java/ru/job4j/conc__store/UserStorage.java
@@ -3,42 +3,34 @@ package ru.job4j.conc__store;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStorage {
     @GuardedBy("this")
-    private final List<User> users = new ArrayList<>();
+    private final Map<Integer, User> users = new HashMap<>();
 
     public synchronized boolean delete(User user) {
-        return users.remove(user);
+        return users.remove(user.getId(), user);
     }
 
     public synchronized boolean update(User user) {
-        for (User value : users) {
-            if (value.getId() == user.getId()) {
-                value.setAmount(user.getAmount());
-                return true;
-            }
+        User tmp = users.get(user.getId());
+        if (tmp != null) {
+            tmp.setAmount(user.getAmount());
+            return true;
         }
         return false;
     }
 
-    public synchronized boolean add(User user) {
-        return users.add(user);
+    public synchronized void add(User user) {
+        users.put(user.getId(), user);
     }
 
     public synchronized boolean transfer(int fromId, int toId, int amount) {
-        User from = null;
-        User to = null;
-        for (User user : users) {
-            if (user.getId() == fromId) {
-                from = user;
-            } else if (user.getId() == toId) {
-                to = user;
-            }
-        }
+        User from = users.get(fromId);
+        User to = users.get(toId);
 
         if (from != null && to != null) {
             int balance = from.getAmount();
@@ -58,7 +50,7 @@ public class UserStorage {
         store.add(new User(2, 200));
 
         store.transfer(1, 2, 50);
-        for (User user : store.users) {
+        for (User user : store.users.values()) {
             System.out.println(user.getId() + " " + user.getAmount());
         }
     }
