91b4cea350ea7701daa687bad4c4e1e715ce2b39
xocer <vitamin9292@mail.ru>
2021-02-04 15:53:39 +0300

    4. ThreadSafe динамический список [#458467]

---
+++ src/main/java/ru/job4j/synch/SimpleArray.java
@@ -0,0 +1,58 @@
+package ru.job4j.synch;
+
+import java.util.*;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private transient Object[] container = new Object[10];
+
+    private int size;
+    private int modCount;
+    private int point = 0;
+    public int getSize() {
+        return size;
+    }
+
+    public void grow(Object[] array) {
+        Arrays.copyOf(array, (int) ((array.length * 1.5) + 1));
+    }
+
+    public Object[] getContainer() {
+        return container;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) container[index];
+    }
+
+    public void add(T model) {
+        if (size == container.length) {
+            grow(container);
+        }
+        container[size++] = model;
+        modCount++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        int expectedModCount = modCount;
+
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < container.length && point != size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) container[point++];
+            }
+        };
+    }
+}
---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,33 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.*;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final SimpleArray<T> array = new SimpleArray<>();
+
+    public synchronized void add(T value) {
+        array.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return array.get(index);
+    }
+
+    public List<T> copy(SimpleArray<T> list) {
+        ArrayList<T> result = new ArrayList<>();
+        for (int i = 0; i < list.getSize(); i++) {
+            result.add((T) list.getContainer()[i]);
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return copy(array).iterator();
+    }
+}
