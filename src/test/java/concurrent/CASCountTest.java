21d2767995f123b94c8d3481fa279f0f3d34a149
xocer <vitamin9292@mail.ru>
2021-02-08 10:30:01 +0300

    0. CAS - операции [#458473]

---
+++ src/test/java/concurrent/CASCountTest.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import ru.job4j.concurrent.CASСount;
+
+public class CASCountTest {
+    @Test
+    public void whenNormalWork() throws InterruptedException {
+        CASСount count = new CASСount();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                count.increment();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                count.increment();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        assertThat(count.get(), is(20));
+    }
+}
