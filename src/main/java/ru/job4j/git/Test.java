198a8a78ab0ddeef6c1141ef4800ba5d1d212f97
xocer <vitamin9292@mail.ru>
2021-02-15 14:28:12 +0300

    3.4. Временное хранение. git stash. first commit

--- src/main/java/ru/job4j/git/Test.java
+++
@@ -1,5 +0,0 @@
-package ru.job4j.git;
-
-public class Test {
-
-}
---
+++ src/main/java/ru/job4j/git/Test2.java
@@ -0,0 +1,7 @@
+package ru.job4j.git;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Кусочек после того, как я сделал stash");
+    }
+}
